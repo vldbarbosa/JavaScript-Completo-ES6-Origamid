f17133bed09f48c17641bd6d3cab3b5961ce2999
Victor Barbosa <vicldb99@gmail.com>
2020-03-17 19:18:57 -0300

    Aula 03 - Números e Operadores

---
+++ NúmerosEOperadores/exercicio.js
@@ -0,0 +1,19 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 35
+console.log(total);
+// Crie duas expressões que retornem NaN
+var nan1 = "a" - 10;
+console.log(nan1);
+var nan2 = "a"*0;
+console.log(nan2);
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+// Incremente o número 5 e retorne o seu valor incrementado
+var num = 5;
+console.log(++num);
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = numero / 2; // NaN (Not a Number)
---
+++ NúmerosEOperadores/script.js
@@ -0,0 +1,27 @@
+var idade = 20;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+
+console.log(idade);
+console.log(gols);
+console.log(pi);
+console.log(exp);
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5 // 4
+
+// OPERADORES ARITMÉTICOS UNÁRIOS
+var incremento = 5;
+console.log(incremento++); //5
+console.log(incremento); //6
+
+var incremento2 = 5;
+console.log(++incremento2); //6
+console.log(incremento2); //6
+
+// Decremento --
