3dbb5af130f0513b9eff96b1ef275ff7239a49ae
Victor Barbosa <vicldb99@gmail.com>
2020-03-19 15:34:40 -0300

    Atribuição E Ternário

---
+++ AtribuicaoETernario/script.js
@@ -0,0 +1,28 @@
+// OPERADORES DE ATRIBUIÇÃO
+
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+// OPERADOR TERNÁRIO
+var idade = 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber) // Pode beber
+
+// condição ? true : false
+
+// IF ABREVIADO
+var possuiFaculdade = true;
+if(possuiFaculdade) console.log('Possui faculdade');
+else console.log('Não possui faculdade');
+
+// ou
+if(possuiFaculdade)
+  console.log('Possui faculdade');
+else
+  console.log('Não possui faculdade');
